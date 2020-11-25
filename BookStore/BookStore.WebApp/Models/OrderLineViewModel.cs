31408936bcfba43d7bde6573e5f220eed32458a3
Antonio Mendez <mendac01@gmail.com>
2020-11-25 10:29:07 -0500

    Added OrderLineViewModel

---
+++ BookStore/BookStore.WebApp/Models/OrderLineViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.WebApp.Models
+{
+    public class OrderLineViewModel
+    {
+        string BookISBN { get; set; }
+    }
+}
